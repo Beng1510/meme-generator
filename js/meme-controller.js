acc2c1b661204e2acbbc7564ab5643e8c62c071b
Ben Graham <benben1510@gmail.com>
2020-10-21 11:45:38 +0300

    added image and text input

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -6,8 +6,9 @@ function init() {
     gCtx = gCanvas.getContext('2d');
     // console.log('The context:', gCtx);
     drawImg()
+    // drawText(gMeme.lines[0].txt, 50, 50)
 }
-console.log('gImgs',gImgs);
+
 var gImgs = [
     {
         id: 1,
@@ -40,14 +41,56 @@ var gImgs = [
     {
         id: 8,
         url: 'img/8.jpg',
+    },
+    {
+        id: 9,
+        url: 'img/9.jpg',
+    },
+    {
+        id: 10,
+        url: 'img/10.jpg',
+    },
+    {
+        id: 11,
+        url: 'img/11.jpg',
+    },
+    {
+        id: 12,
+        url: 'img/12.jpg',
+    },
+    {
+        id: 13,
+        url: 'img/13.jpg',
+    },
+    {
+        id: 14,
+        url: 'img/14.jpg',
+    },
+    {
+        id: 15,
+        url: 'img/15.jpg',
+    },
+    {
+        id: 16,
+        url: 'img/16.jpg',
+    },
+    {
+        id: 17,
+        url: 'img/17.jpg',
+    },
+    {
+        id: 18,
+        url: 'img/18.jpg',
     }
 
 ]
+console.log('gImgs',gImgs);
+// console.log('gImgs[0].url;',gImgs[0].url);
 
 var gMeme = {
-    selectedImgId: 5,
+    selectedImgId: 14,
     selectedLineIdx: 0,
-
+    
     lines: [
         {
             txt: 'Love and Peace',
@@ -56,4 +99,19 @@ var gMeme = {
             color: 'blue'
         }
     ]
+}
+console.log('gMeme.lines[0].txt',gMeme.lines[0].txt);
+
+function onUserTxtInput() {
+    console.log('text',text);
+
+    var text = UserTxtInput();
+
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = gMeme.lines[0].color
+    gCtx.font = '24px IMPACT';
+    gCtx.textAlign = gMeme.lines[0].align
+    gCtx.fillText(text, 50, 50)
+    gCtx.strokeText(text, 50, 50)
+
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,8 +1,47 @@
+// function drawImg() {
+//     console.log('lets draw an img');
+//     var img = new Image()
+//     img.src = gImgs[gMeme.selectedImgId].url;
+//     img.onload = () => {
+//         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+//     }
+// }
 function drawImg() {
-    console.log('lets draw img');
+    console.log('lets draw an img');
     var img = new Image()
-    img.src = gImgs.url;
     img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-    }
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+        // gCtx.strokeStyle = 'black'
+        // gCtx.fillStyle = 'white'
+        // gCtx.font = '24px IMPACT';
+        // gCtx.fillText(gMeme.lines[0].txt, 50, 50)
+        // gCtx.strokeText(gMeme.lines[0].txt, 50, 50)
+    };
+    img.src = gImgs[gMeme.selectedImgId].url;
+    
+}
+
+
+// function drawText(text, x, y) {
+//     console.log('lets write');
+//     gCtx.strokeStyle = 'red'
+//     gCtx.fillStyle = 'white'
+//     gCtx.lineWidth = '2'
+//     gCtx.font = '24px Ariel'
+//     gCtx.textAlign = 'start'
+//     gCtx.fillText(text, x, y)
+//     gCtx.strokeText(text, x, y)
+// }
+
+
+
+function UserTxtInput() {
+    console.log('hi');
+    let text = document.getElementById('userTxt').value;
+    // gCtx.strokeStyle = 'black'
+    //     gCtx.fillStyle = 'white'
+    //     gCtx.font = '24px IMPACT';
+        // gCtx.fillText(text, 50, 50)
+        // gCtx.strokeText(text, 50, 50)
+return text
 }
